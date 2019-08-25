ca03863a819993a8f9a69033173e24b1403b225f
aovlexus <aovlexus@gmail.com>
2019-08-25 20:21:56 +0700

    Fix trailing slash

--- ya_test/imports/urls.py
+++ ya_test/imports/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 
 from imports.views import ImportViewSet
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register(r'imports', ImportViewSet, basename='imports')
 
 
